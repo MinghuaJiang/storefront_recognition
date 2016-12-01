735f42af3aaf64abd6fae2c9c099c1472cbd5e7f
Minghua Jiang <61221630@qq.com>
2016-12-01 02:07:27 -0500

    add demo

--- src/storefront_service.py
+++ src/storefront_service.py
@@ -1,4 +1,11 @@
 import storefront_recognizer
+from flask import Flask
+from flask import request
 
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/business', methods=['POST'])
 def generate_business_info(image, latitude, longitude):
     return storefront_recognizer.get_business_info(image, latitude, longitude)
