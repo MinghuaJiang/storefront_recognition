97650f16d47ca54e38475ef54587186f084f910f
Minghua Jiang <61221630@qq.com>
2016-12-02 03:33:01 -0500

    文件 wait_delete.txt 现在终于删掉了

--- src/cifar-config.py
+++
@@ -1,7 +0,0 @@
-{
-    'verbose': True,
-    'from_pickle': False, 
-    'pickle_data': '../model/images-50000-(20, 20).pickle',
-    'folder': '../data/CIFAR',
-    'img_size': (20, 20),
-}
--- src/preprocessing.py
+++
@@ -1,6 +0,0 @@
-import pandas
-import numpy
-
-
-def load_data(path):
-    return 1
